1b0d77efe2729bcdc239fe3ae5160778bb411ea4
exconf <sergey.sosedkin@gmail.com>
2016-09-28 00:03:56 +0300

    user.ip fixed

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express')
 var app = express()
 var port = process.env.PORT || 8080
-var ip, lang, system, ip2
+var ip, lang, system
+
 
 app.get('/', function(req, res) {
-  ip = req.connection.remoteAddress
-  ip2 = req.headers['x-forwarded-for']
+  ip = req.headers['x-forwarded-for']
   lang = req.headers['accept-language'].split(',')[0]
   system = req.headers['user-agent'].match(/\((.*?)\)/)[1]
   //^^parse user-agent and take what is between parentheses
-  //console.log(ip + '\n' + lang + '\n' + system)
-  var output = { 'ipaddress': ip, 'language': lang, 'software': system, 'ip2': ip2 }
+  console.log(ip + '\n' + lang + '\n' + system)
+  var output = { 'ipaddress': ip, 'language': lang, 'software': system }
   res.send(output)
 })
 
